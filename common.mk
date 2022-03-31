0d08b3fe811b30c39d9fce2f8b14290c8a979b45
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-03-31 20:37:52 +0530

    sm6250-common: permissions: Enable Google Dialer call recording feature
    Signed-off-by: itzuday2312 <udaybindal2312@gmail.com>

--- common.mk
+++ common.mk
@@ -209,6 +209,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
+# Google Dialer Call recording
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # Google LILY_EXPERIENCE feature
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/pixel/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml
