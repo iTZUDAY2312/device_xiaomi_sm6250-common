ecb62bb0afda668d8f48a96e538f43bddac7219b
someone5678 <nemui3353@gmail.com>
2022-06-30 03:05:39 +0900

    sm6250-common: permissions: Enable google LILY_EXPERIENCE feature
    * Extracted from Google Camera Go official release 3.6.455515391
    * Checked from Google Camera Go, Gallery Go
    * It enables several Android Go specific features for Android Go Apps
    
    Example:
    Google Camera Go:
    * HDR, Night Mode, Snapchat Sticker integration
    
    Change-Id: I93cc1070bec2e6b7c31440f06232255913592c6c
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: itzuday2312 <udaybindal2312@gmail.com>

--- common.mk
+++ common.mk
@@ -210,7 +210,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
-    
+
+# Google LILY_EXPERIENCE feature
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml
+
 # Google Photos
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/pixel/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
