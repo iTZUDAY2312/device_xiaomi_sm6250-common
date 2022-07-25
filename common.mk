53bb418f1bd0d6a8a55a8b43f686b3d6d41cebf8
Sonal Singh <sonal.singh.19993@gmail.com>
2022-07-25 21:37:41 +0530

    sm6250-common: Switch to GCamGOPrebuilt-V3
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -116,7 +116,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     libgui_vendor \
-    Snap
+    GCamGOPrebuilt-V3
 
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
