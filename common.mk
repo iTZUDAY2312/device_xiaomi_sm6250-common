c908fd352e2cb628a5be86e37825c0a70725f930
iTZ_UDAY2312 <72090728+UdayBindal2312@users.noreply.github.com>
2022-09-10 12:24:53 +0530

    Add Miui Cam Conditionally

--- common.mk
+++ common.mk
@@ -16,6 +16,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# MiuiCamera
+ifeq ($(SHIP_ANX), true)
+$(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
+endif
+
 # Enforce native interfaces of product partition as VNDK
 PRODUCT_PRODUCT_VNDK_VERSION := current
 
